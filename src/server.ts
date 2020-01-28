25b790bd411a5d25a55ef4f0e908b0526eae8697
Revant <revantjha.94@gmail.com>
2020-01-28 15:23:48 +0530

    added basic swagger setup

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify"
 import { Server, IncomingMessage, ServerResponse } from "http"
 import config from './config'
 import logger from './logger'
+import swagger from 'fastify-swagger'
 
 
 // If using http2 we'd pass <http2.Http2Server, http2.Http2ServerRequest, http2.Http2ServerResponse>
@@ -9,6 +10,22 @@ const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> =
     // Instead of using Fastify default logger use our custom logger internally
     logger: logger
 })
+
+const swaggerOption = {
+    swagger: {
+        info: {
+            title: 'Bizgaze Email Server',
+            description: 'Email Server API Docs',
+            version: '1.0.0'
+        },
+        host: 'localhost',
+        schemes: ['http', 'https'],
+        consumes: ['application/json'],
+        produces: ['application/json']
+    }
+}
+
+server.register(swagger, swaggerOption)
 
 
 const startHTTPServer = async () => {
