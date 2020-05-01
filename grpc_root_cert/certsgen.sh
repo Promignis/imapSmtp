7ff6752088877902344964b8b2e8eee90adb9afc
Revant <revantjha.94@gmail.com>
2020-05-01 15:48:57 +0530

    updated local grpc tls certs

--- grpc_root_cert/certsgen.sh
+++ grpc_root_cert/certsgen.sh
@@ -5,14 +5,14 @@ echo "Generating certificates ..."
 #create root CA cert
 openssl genrsa -out bizgaze.root.key 2048
 
-openssl req -new -x509 -key bizgaze.root.key -out bizgaze.root.crt -subj  "/CN=ca"
+openssl req -new -x509 -key bizgaze.root.key -out bizgaze.root.crt -subj  "/CN=ca" -days 365
 
 # create server
 openssl genrsa -out server.key 4096
 
 openssl req -new -key server.key -out server.csr -subj  "/CN=localhost"
 
-openssl x509 -req -in server.csr -CA bizgaze.root.crt -CAkey bizgaze.root.key -set_serial 01  -out server.crt
+openssl x509 -req -in server.csr -CA bizgaze.root.crt -CAkey bizgaze.root.key -set_serial 01  -out server.crt -days 365
 
 # client
 
@@ -20,4 +20,4 @@ openssl genrsa -out client.key 4096
 
 openssl req -new -key client.key -out client.csr -subj  "/CN=localhost"
 
-openssl x509 -req -in client.csr -CA bizgaze.root.crt -CAkey bizgaze.root.key -set_serial 01  -out client.crt
+openssl x509 -req -in client.csr -CA bizgaze.root.crt -CAkey bizgaze.root.key -set_serial 01  -out client.crt -days 365
